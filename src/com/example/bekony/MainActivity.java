a2de1563e672baeb8afbf45ef79c456af9278f3d
TeMPOraL <temporal.pl@gmail.com>
2014-11-22 03:31:33 +0100

    Initial commit.

---
+++ src/com/example/bekony/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.bekony;
+
+import com.kontakt.sdk.android.configuration.ForceScanConfiguration;
+import com.kontakt.sdk.android.configuration.MonitorPeriod;
+import com.kontakt.sdk.android.connection.OnServiceBoundListener;
+import com.kontakt.sdk.android.device.Region;
+import com.kontakt.sdk.android.manager.BeaconManager;
+import com.kontakt.sdk.android.device.Beacon;
+import java.util.List;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+	
+    private static final int REQUEST_CODE_ENABLE_BLUETOOTH = 1;
+
+    private BeaconManager beaconManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        beaconManager = BeaconManager.newInstance(this);
+        beaconManager.setMonitorPeriod(MonitorPeriod.MINIMAL);
+        beaconManager.setForceScanConfiguration(ForceScanConfiguration.DEFAULT);
+        beaconManager.registerMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onMonitorStart() {}
+
+            @Override
+            public void onMonitorStop() {}
+
+            @Override
+            public void onBeaconsUpdated(final Region region, final List<Beacon> beacons) {}
+
+            @Override
+            public void onBeaconAppeared(final Region region, final Beacon beacon) {}
+
+            @Override
+            public void onRegionEntered(final Region region) {}
+
+            @Override
+            public void onRegionAbandoned(final Region region) {}
+        });
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(!beaconManager.isBluetoothEnabled()) {
+            final Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, REQUEST_CODE_ENABLE_BLUETOOTH);
+        } else {
+            connect();
+        }
+    }
+    
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        beaconManager.stopMonitoring();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.disconnect();
+        beaconManager = null;
+    }
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == REQUEST_CODE_ENABLE_BLUETOOTH) {
+            if(resultCode == Activity.RESULT_OK) {
+                connect();
+            } else {
+                Toast.makeText(this, "Bluetooth not enabled", Toast.LENGTH_LONG).show();
+                getActionBar().setSubtitle("Bluetooth not enabled");
+            }
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void connect() {
+        try {
+            beaconManager.connect(new OnServiceBoundListener() {
+                @Override
+                public void onServiceBound() {
+                    try {
+                        beaconManager.startMonitoring(Region.EVERYWHERE);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (RemoteException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
